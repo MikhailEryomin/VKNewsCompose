e20abbbf5eaa05c5065185b7a800915c65b72e08
Mikhailer <eremix2017@yandex.ru>
2026-05-28 21:58:21 +0300

    lesson 1: postcard

--- app/src/main/java/com/example/vknewscompose/MainActivity.kt
+++ app/src/main/java/com/example/vknewscompose/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.vknewscompose.ui.theme.PostCard
 import com.example.vknewscompose.ui.theme.VkNewsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,19 +37,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            VkNewsLayout()
+            VkNewsComposeTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(WindowInsets.statusBars.asPaddingValues())
+                ) {
+                    PostCard()
+                }
+            }
         }
     }
 }
-
-@Preview
-@Composable
-fun VkNewsPreview() {
-
-}
-
-@Composable
-fun VkNewsLayout() {
-
-}
-
