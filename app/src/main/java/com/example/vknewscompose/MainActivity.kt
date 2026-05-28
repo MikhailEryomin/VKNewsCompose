a9d4a633e0c7a6766f8b22763c87f6bd8a89b108
Mikhailer <eremix2017@yandex.ru>
2026-05-28 20:46:48 +0300

    initial

---
+++ app/src/main/java/com/example/vknewscompose/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.vknewscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.vknewscompose.ui.theme.VkNewsComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VkNewsLayout()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun VkNewsPreview() {
+
+}
+
+@Composable
+fun VkNewsLayout() {
+
+}
+
